2e256df0507bcbb90b9921e9c117bf5d3dc76639
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-03 00:03:52 +0200

    added , {useNewUrlParser: true}

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function () {
+app.listen(5000, function () {
     console.log("Server started on port 3000");
 });
