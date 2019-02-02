d41ab8ad1e02c7b25a0e5670d315a2504f7ae7d3
pyavchik@gmail.com <pyavchik@gmail.com>
2019-02-02 23:48:38 +0200

    added , {useNewUrlParser: true}

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb://pyavchik:XRszRDBPsExh55Q@ds119795.mlab.com:19795/deployingdemo");
+mongoose.connect("mongodb://pyavchik:XRszRDBPsExh55Q@ds119795.mlab.com:19795/deployingdemo", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
